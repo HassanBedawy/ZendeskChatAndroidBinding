302467edfe9ca008f5852148997eb858e40b3d65
Hassan Bedawy <hassan.bedawy@outlook.com>
2019-05-14 11:34:33 +0100

    Add project files.

---
+++ ZendeskAndroidTest/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Com.Zopim.Android.Sdk.Api;
+using Com.Zopim.Android.Sdk.Prechat;
+
+namespace ZendeskAndroidTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            ZopimChatApi.Init("MtQ7uqHGiMKkIBNSfnQnEs97auURAt55");
+            Intent intent = new Intent(this, typeof(ZopimChatActivity));  
+            StartActivity(intent);  
+            
+            //var zopimChat = new ZopimChat();
+            //zopimChat.init("");
+        }
+	}
+}
+
